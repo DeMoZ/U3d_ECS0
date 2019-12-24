77ff61bba46ab86fb0b8d2e234a451ed910d6cec
Roman Kolychev <demozbox@gmail.com>
2019-12-25 00:12:26 +0300

    following https://www.youtube.com/watch?time_continue=108&v=WZ6-LxwxWEI&feature=emb_logo code writen. required some polishing to get visible result

---
+++ Assets/Scripts/MovementJob.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using UnityEngine.Jobs;
+using System;
+
+//https://www.youtube.com/watch?time_continue=108&v=WZ6-LxwxWEI&feature=emb_logo
+
+//[ComputeJobOptimisation]
+public struct MovementJob : IJobParallelForTransform
+{
+    public float moveSpeed;
+    public float boundTop;
+    public float boundBottom;
+    public float deltatime;
+
+    public void Execute(int index, TransformAccess transform)
+    {
+        //Vector3 pos = transform.position;
+        //pos += moveSpeed * deltatime * (transform.rotation * Vector3.up);
+
+        //if (pos.z < boundBottom)
+        //{
+        //    pos.z = boundTop;
+        //}
+
+        //transform.position = pos;
+
+        transform.rotation=Quaternion.Euler(transform.rotation.x+ moveSpeed * deltatime, transform.rotation.y, transform.rotation.z);
+    }
+
+    
+}
