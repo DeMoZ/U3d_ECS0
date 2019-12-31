d79b9b60fcdb922c57e558c41ece0ed6fae20673
Roman Kolychev <kolychevra@sibintek.ru>
2019-12-31 09:05:35 +0300

    Burst. also depricated way

---
+++ Assets/Scripts/Burst/MovementBurst.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://www.youtube.com/results?search_query="unite+copenhagen",+long
+public class MovementBurst : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
