54d0a6c954086ac5538024e0475cecfbd0920760
Roman Kolychev <demobox@gmail.com>
2020-11-02 00:20:49 +0300

    Attack, Chase and Patroll systems modified. Behaviour system removed.

--- Assets/Scripts/ECS/Systems/ChaseSystem.cs
+++ Assets/Scripts/ECS/Systems/ChaseSystem.cs
@@ -6,12 +6,11 @@ public class ChaseSystem : ComponentSystem
 {
     protected override void OnUpdate()
     {
+        float deltaTime = Time.DeltaTime;
+
         Entities.WithAll<BehaviourStateChasing>().ForEach((
-            Entity entity,
-            ref Translation translation,
-            ref Rotation rotation,
-            ref BehaviourStateChasing behaviourStateChasing,
-            ref Turning turning
+            Entity entity, ref Translation translation, ref Rotation rotation, ref BehaviourStateChasing behaviourStateChasing,
+            ref Turning turning, ref Speed speed, ref AttackDistance attackDistance
             ) =>
         {
             float3 myPos = translation.Value;
@@ -24,6 +23,24 @@ public class ChaseSystem : ComponentSystem
 
                 // rotation
                 rotation.Value = SharedMethods.RotateTowardsTarget(myPos, myRot, turgetPoint, turningSpeed, Time.DeltaTime);
+
+                // movement
+                translation.Value += speed.PatrollSpeed * deltaTime * math.forward(rotation.Value);
+
+                // checks for attack
+                float3 targetPos = World.DefaultGameObjectInjectionWorld.EntityManager.GetComponentData<Translation>(behaviourStateChasing.ChaseTarget).Value;
+                float myAttackDistance = attackDistance.Value;
+
+                if (SharedMethods.CanAttackTarget(myPos, targetPos, myAttackDistance))
+                {
+                    EntityManager.RemoveComponent(entity, typeof(BehaviourStateChasing));
+                    EntityManager.AddComponentData(entity, new BehaviourStateAttacking { AttackTarget = behaviourStateChasing.ChaseTarget });
+                }
+            }
+            else
+            {
+                EntityManager.RemoveComponent(entity, typeof(BehaviourStateChasing));
+                EntityManager.AddComponent(entity, typeof(BehaviourStatePatrolling));
             }
         });
     }
