f97c48e5ca841a12b396270caa40b497986f33de
Roman Kolychev <demobox@gmail.com>
2020-11-15 23:19:55 +0300

    Enemy amount showed on UI on Add.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -25,15 +25,16 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private float _spawnRadius = 10f;
     [SerializeField] private MainUI _mainUI;
 
-    private EntityManager entityManager;
+    private EntityManager _entityManager;
 
     private void Start()
     {
         _spawnTimer = _spawnTime;
 
-        entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
 
         var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+        _mainUI.SetAddAmoutn(_manualSpawnAmount);
 
         foreach (GameObject prefab in _enemiesPrefabs)
         {
@@ -67,35 +68,56 @@ public class EnemySpawner : MonoBehaviour
             tagCount++;
             tagCount = tagCount >= _enemyEntitiesPrefabs.Count ? 0 : tagCount;
         }
+
+        CountEntities();
     }
 
     private void SpawnEntity(int teamTag)
     {
-        Entity entity = entityManager.Instantiate(_enemyEntitiesPrefabs[teamTag]);
+        Entity entity = _entityManager.Instantiate(_enemyEntitiesPrefabs[teamTag]);
 
-        entityManager.AddComponent<OnTheScene>(entity);
-        entityManager.AddComponent<BehaviourStatePatrolling>(entity);
-        entityManager.SetComponentData(entity, new Translation { Value = SharedMethods.RandomPointOnCircle(_spawnRadius) });
-        entityManager.SetComponentData(entity, new Rotation { Value = SharedMethods.RandomRotation() });
-        ShootTimer st = entityManager.GetComponentData<ShootTimer>(entity);
+        _entityManager.AddComponent<OnTheScene>(entity);
+        _entityManager.AddComponent<BehaviourStatePatrolling>(entity);
+        _entityManager.SetComponentData(entity, new Translation { Value = SharedMethods.RandomPointOnCircle(_spawnRadius) });
+        _entityManager.SetComponentData(entity, new Rotation { Value = SharedMethods.RandomRotation() });
+        ShootTimer st = _entityManager.GetComponentData<ShootTimer>(entity);
         st.TimerCounter = SharedMethods.MakeRandom(st.TimeRange);
-        entityManager.SetComponentData(entity, st);
+        _entityManager.SetComponentData(entity, st);
     }
 
 
     public void MinusEntities()
     {
-        NativeArray<Entity> enemyArray = entityManager.GetAllEntities(Allocator.Temp);
+        NativeArray<Entity> enemyArray = _entityManager.GetAllEntities(Allocator.Temp);
         Debug.Log($"enemies amount {enemyArray.Length}");
         // int removeAmount = Mathf.Clamp(enemyArray.Length - _manualSpawnAmount, 0, enemyArray.Length);
         int removeAmount = enemyArray.Length - 1;
 
         for (int i = removeAmount; i > -1; i--)
-            if (entityManager.HasComponent<OnTheScene>(enemyArray[i]))
-                entityManager.DestroyEntity(enemyArray[i]);
+            if (_entityManager.HasComponent<OnTheScene>(enemyArray[i]))
+            {
+                _entityManager.DestroyEntity(enemyArray[i]);
 
-        enemyArray = entityManager.GetAllEntities(Allocator.Temp);
-        _mainUI.SetTotalEnemyValue(enemyArray.Length);
+            }
+        enemyArray = _entityManager.GetAllEntities(Allocator.Temp);
+        _mainUI.SetTotalEnemyValue(0);
         enemyArray.Dispose();
+    }
+
+    private void CountEntities()
+    {
+        NativeArray<Entity> entitiesArray = _entityManager.GetAllEntities(Allocator.Temp);
+        var count = 0;
+
+        for (int i = 0; i < entitiesArray.Length; i++)
+        {
+            if (_entityManager.HasComponent<OnTheScene>(entitiesArray[i]))
+            {
+                count++;
+            }
+        }
+        entitiesArray.Dispose();
+
+        _mainUI.SetTotalEnemyValue(count);
     }
 }
--- Assets/Scripts/MainUI.cs
+++ Assets/Scripts/MainUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,31 @@ using UnityEngine.UI;
 
 public class MainUI : MonoBehaviour
 {
-    [SerializeField] Text _totalEnemy ;
-    
+    [SerializeField] Text _totalEnemy;
+    [SerializeField] Text _addBtn;
+
+    public static MainUI instance;
+
+    private int _totalValue = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     public void SetTotalEnemyValue(int value)
     {
+        _totalValue = value;
         _totalEnemy.text = value.ToString();
+    }
+
+    internal void SetAddAmoutn(int manualSpawnAmount)
+    {
+        _addBtn.text = string.Concat("Add ", manualSpawnAmount);
+    }
+
+    internal void DecreaseEnemyValue(int amount)
+    {
+        _totalEnemy.text = (_totalValue - 1).ToString();
     }
 }
