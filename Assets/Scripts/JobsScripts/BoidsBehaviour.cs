e3b49ad0daab005ef948856626710041b10b202c
Roman Kolychev <demobox@gmail.com>
2020-12-23 23:37:25 +0300

    Simple moving with jobs implemented.

---
+++ Assets/Scripts/JobsScripts/BoidsBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Jobs;
+using Random = UnityEngine.Random;
+
+namespace JobsScripts
+{
+    public class BoidsBehaviour : MonoBehaviour
+    {
+        [SerializeField] private int _numberOfEntities;
+        [SerializeField] private GameObject _entityPrefab;
+
+        [Tooltip("GroundHeight")]
+        private float _groundHeight = 0;
+        
+        private NativeArray<Vector3> _positions;
+        private NativeArray<Vector3> _velocities;
+
+        private TransformAccessArray _transformAccessArray; // структура принимает в конструктор массив трансформов
+
+        private void Start()
+        {
+            _positions = new NativeArray<Vector3>(_numberOfEntities, Allocator.Persistent);
+            _velocities = new NativeArray<Vector3>(_numberOfEntities, Allocator.Persistent);
+
+            var transforms = new Transform[_numberOfEntities];
+
+            for (int i = 0; i < _numberOfEntities; i++)
+            {
+                transforms[i] = Instantiate(_entityPrefab).transform;
+                var random=Random.insideUnitCircle;
+                _velocities[i] = new Vector3(random.x, _groundHeight, random.y);
+            }
+
+            _transformAccessArray = new TransformAccessArray(transforms); // потокобезопасная обертка
+        }
+
+        private void Update()
+        {
+            var moveJob = new MoveJob()
+            {
+                Positions = _positions,
+                Velocities = _velocities,
+                DeltaTime = Time.deltaTime
+            };
+
+            var jobHandle = moveJob.Schedule(_transformAccessArray);    
+            jobHandle.Complete();       // основной поток подождет выполнения этой задачи
+        }
+
+        private void OnDestroy()
+        {
+            _positions.Dispose();
+            _velocities.Dispose();
+            _transformAccessArray.Dispose();
+        }
+    }
+}
---
+++ Assets/Scripts/JobsScripts/MoveJob.cs
@@ -0,0 +1,35 @@
+﻿using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+namespace JobsScripts
+{
+    public struct MoveJob : IJobParallelForTransform
+    {
+        public NativeArray<Vector3> Positions; // позиции всех трансформов
+        public NativeArray<Vector3> Velocities; // хранение актуальных векторов движения для каждой из сущностей
+        public float DeltaTime;
+
+        public void Execute(int index, TransformAccess transform)
+        {
+            var velocity = Velocities[index]; // переменная для удобства использования
+            transform.position += velocity * DeltaTime;
+            transform.rotation = Quaternion.LookRotation(velocity);
+
+            // сохраняю позицию в массиве
+            Positions[index] = transform.position;
+        }
+    }
+
+    public struct AccelerationJob : IJobParallelForTransform
+    {
+        [ReadOnly] public NativeArray<Vector3> Positions;
+        [ReadOnly] public NativeArray<Vector3> Velocities;
+        [WriteOnly] public NativeArray<Vector3> Acceleratioins;
+
+        public void Execute(int index, TransformAccess transform)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
